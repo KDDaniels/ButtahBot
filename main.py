d5425211996f2e5782b3d716b5ad86d7b2ecfdba
AgehWewt <kendall.daniels.dev@gmail.com>
2024-12-17 23:26:05 -0600

    updated terminal output colors

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python
 
+"""
+
+"""
+
 import os
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'\033[92m[INFO]\033[0m We have logged in as \033[96m{bot.user}\033[0m')
+
+    # Loads all of the cogs in the ./cogs folder
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            try:
+                await bot.load_extension(f'cogs.{filename[:-3]}')
+                print(f'\033[92m[INFO]\033[0m Loaded cog from \033[96m{filename}\033[0m')
+            except Exception as e:
+                print(f'\033[91m[ERROR]\033[0m Failed to load cog in \033[96m{filename}\033[0m: {e}')
+
+    print(f'\033[92m[INFO]\033[0m Cogs done loading.')
+
+@bot.listen()
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
 if __name__ == "__main__":
-    intents = discord.Intents.default()
-    intents.message_content = True
-
-    bot = commands.Bot(command_prefix='!', intents=intents)
-
-    @bot.event
-    async def on_ready():
-        for filename in os.listdir("./cogs"):
-            if filename.endswith(".py"):
-                try:
-                    await bot.load_extension(f'cogs.{filename[:-3]}')
-                    print(f'[INFO] Loaded cog {filename[:-3]}')
-                except Exception as e:
-                    print(f'[ERROR] Failed to load cog {filename[:-3]}: {e}')
-
-        
-        print(f'We have logged in as {bot.user}')
-
-    bot.run(os.environ["BUTTAHBOT_TOKEN"])
+    try:
+        bot.run(os.environ["BUTTAHBOT_TOKEN"])
+    except Exception as e:
+        print(f'\033[91m[ERROR]\033[0m Error: {e}')
