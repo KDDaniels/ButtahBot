d8d7f46f7f7b657621f11dd0424bdf817655a4e6
AgehWewt <kendall.daniels.dev@gmail.com>
2024-12-19 01:03:16 -0600

    updated run_docker to build as well

--- run_docker.py
+++ run_docker.py
@@ -3,8 +3,19 @@ import subprocess
 
 if __name__ == "__main__":
     """
-    Runs docker with all of the required commands
+    Builds and then runs docker with all of the required commands
     """
+    try:
+        subprocess.run([
+            "docker",
+            "build",
+            "-t",
+            "buttahbot",
+            "."
+        ])
+    except Exception as e:
+        print(f"[ERROR] {e}")
+
     try:
         token = os.environ["BUTTAHBOT_TOKEN"]
         subprocess.run([
