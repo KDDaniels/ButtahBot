79278ad4db5e9973d72b30cae87d25e8be0a6429
AgehWewt <kendall.daniels.dev@gmail.com>
2024-12-30 15:21:38 -0600

    added arguments to just run the docker image instead of building everytime

---
+++ run.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    """
+    Builds and then runs Docker with all of the required commands
+
+    Args:
+        -b or -B: builds the Docker image
+    """
+    if len(sys.argv) == 1:
+        try:
+            token = os.environ["BUTTAHBOT_TOKEN"]
+            subprocess.run([
+                "docker",
+                "run",
+                "--rm",
+                "-e",
+                f"BUTTAHBOT_TOKEN={token}",
+                "-e",
+                "PYTHONUNBUFFERED=1",
+                "buttahbot"
+            ])
+        except Exception as e:
+            print(f"[ERROR] {e}")
+    
+    elif (len(sys.argv) == 1 and (sys.argv[1] == "-b" or sys.argv[1] == "-B")):
+        try:
+            subprocess.run([
+                "docker",
+                "build",
+                "-t",
+                "buttahbot",
+                "."
+            ])
+        except Exception as e:
+            print(f"[ERROR] {e}")
+
+    else:
+        print(f"[WARN] Unknown argument")
