55acaf600e920384a0ec697f1db7ae702ea108c2
AgehWewt <kendall.daniels.dev@gmail.com>
2024-12-30 15:21:58 -0600

    added license text and some docs

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 
 """
+    ButtahBot is a hobby Discord bot to just do goofy stuff with.
+    Copyright (C) 2024 Kendall Daniels
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+    
+    Fun little Discord bot I've made for my friends and I to goof around with,
+    probably won't add anything too complicated unless it sounds neat (e.g. TTS)
 """
 
 import os
